44de2b3035cc236c8f4dd6c39d832e09ec279e76
t-shibata <shibazou3@gmail.com>
2021-02-22 14:08:42 +0900

    change api settings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+require("dotenv").config({ path: "./.env" });
 module.exports = {
   images: {
     domains: ["images.microcms-assets.io"],
+  },
+  env: {
+    X_API_KEY: process.env.X_API_KEY,
   },
 };
